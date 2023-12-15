af42c317b899013227adf339da32db9a40add30b
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-12-15 09:42:27 -0300

    first commit

---
+++ src/modules/personagem.js
@@ -0,0 +1,22 @@
+export class Personagem {
+    nome
+    vida
+    mana
+    level
+    tipo
+    descricao
+
+    constructor(nome, level, tipo) {
+        this.nome = nome
+        this.level = level
+        this.tipo = tipo
+    }
+
+    obterInsignia() {
+        if (this.level >= 5) {
+            return `Implacavel ${this.tipo}`
+        }
+
+        return `${this.tipo} Iniciante`
+    }
+}
