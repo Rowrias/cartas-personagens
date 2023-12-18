d9da1590bbddd03a19408f29436c3ff4d19c7b14
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-12-18 15:01:58 -0300

    3.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/personagem.js"
+import { arqueiro } from "./modules/arqueiro.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
+const arqueiroBruno = new arqueiro('Bruno', 7, 8)
 
-const personagens = [magoAntonio, magaJulia]
+const personagens = [magoAntonio, magaJulia, arqueiroBruno]
 
 new PersonagemView(personagens).render()
